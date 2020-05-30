2fca3558b3d286f3186b88895d28616f0b4a2223
microbitsarecool <66143785+microbitsarecool@users.noreply.github.com>
2020-05-30 14:02:07 -0600

    Updated linux_deb installer!
    Updated how path is added

--- linux_deb_install.sh
+++ linux_deb_install.sh
@@ -8,6 +8,6 @@ mv * /home/"$USER"/KShell/
 echo "Successfully moved files to directory /home/$USER/KShell/"
 cd /home/"$USER"/KShell/
 echo "You are now in the home directory of your KShell application!"
-echo -e "export PATH=\x22$PATH\x22:/home/$USER/KShell/dist/kshell" >> ~/.bashrc
+echo "export PATH='$PATH':/home/$USER/KShell/dist/kshell" >> ~/.bashrc
 echo "Launching KShell... to run, use the 'kshell' command from any directory in Terminal!"
 kshell
