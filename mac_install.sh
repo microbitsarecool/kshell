e1a75e9d9f52ea1f144a8754ccbeccb5ea0c316c
microbitsarecool <66143785+microbitsarecool@users.noreply.github.com>
2020-05-30 00:12:47 -0600

    Updated mac_install.sh!
    Updated compatibility for kshell executable

--- mac_install.sh
+++ mac_install.sh
@@ -6,11 +6,11 @@ brew install python3
 brew install pip3
 pip3 install tkinter
 pip3 install pyinstaller
-pyinstaller kshell_base.py
+pyinstaller kshell.py
 mv * /home/"$user"/KShell
 echo "Successfully moved files to directory /home/$USER/KShell/"
 cd /home/"$user"/KShell
 echo "You are now in the home directory of your KShell application!"
-echo "export PATH=/home/$USER/KShell/dist/kshell_base:$PATH" >> ~/.profile
-echo "Launching KShell... to run, use the 'kshell_base' command from any directory in Terminal!"
-kshell_base
+echo "export PATH=/home/$USER/KShell/dist/kshell:$PATH" >> ~/.profile
+echo "Launching KShell... to run, use the 'kshell' command from any directory in Terminal!"
+kshell
