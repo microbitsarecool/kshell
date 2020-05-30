9147c4923753a89fc5a968d7d80638252e8c809b
microbitsarecool <66143785+microbitsarecool@users.noreply.github.com>
2020-05-30 13:38:01 -0600

    Updated linux_suse_install.sh!
    Added updated path...

--- linux_suse_install.sh
+++ linux_suse_install.sh
@@ -1,13 +1,13 @@
-mkdir /home/$USER/KShell
+mkdir /home/"$USER"/KShell
 sudo zypper install python3
 sudo zypper install python3-tk
 sudo zypper install python3-pip
-pip3 install pyinstaller
+sudo pip3 install pyinstaller
 pyinstaller kshell.py
-mv * /home/$USER/KShell
+mv * /home/"$USER"/KShell
 echo "Successfully moved files to directory /home/$USER/KShell/"
-cd /home/$USER/KShell/
+cd /home/"$USER"/KShell/
 echo "You are now in the home directory of your KShell application!"
-echo "export PATH=/home/$USER/KShell/dist/kshell:$PATH" >> ~/.bashrc
+echo -e "export PATH=\x22$PATH\x22:/home/$USER/KShell/dist/kshell" >> ~/.bashrc
 echo "Launching KShell... to run, use the 'kshell' command from any directory in Terminal!"
 kshell
