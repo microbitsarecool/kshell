c38895ada6ab85bb7f43f6adb7b69f5d7b90589a
microbitsarecool <66143785+microbitsarecool@users.noreply.github.com>
2020-05-30 00:08:09 -0600

    Updated linux_deb_install.sh
    Updated compatibility for kshell executable

--- linux_deb_install.sh
+++ linux_deb_install.sh
@@ -3,11 +3,11 @@ sudo apt install python3
 sudo apt install python3-tk
 sudo apt install python3-pip
 pip3 install pyinstaller
-pyinstaller kshell_base.py
+pyinstaller kshell.py
 mv * /home/"$USER"/KShell/
 echo "Successfully moved files to directory /home/$USER/KShell/"
 cd /home/"$USER"/KShell/
 echo "You are now in the home directory of your KShell application!"
-echo "export PATH=/home/$USER/KShell/dist/kshell_base:$PATH" >> ~/.bashrc
-echo "Launching KShell... to run, use the 'kshell_base' command from any directory in Terminal!"
-kshell_base
+echo "export PATH=/home/$USER/KShell/dist/kshell:$PATH" >> ~/.bashrc
+echo "Launching KShell... to run, use the 'kshell' command from any directory in Terminal!"
+kshell
