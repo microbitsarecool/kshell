21d2fed166e0728d0cefa6afc952d7a175666bb0
microbitsarecool <66143785+microbitsarecool@users.noreply.github.com>
2020-05-30 13:05:50 -0600

    Updated linux_rpm_install.sh!
    Updated how to export path

--- linux_rpm_install.sh
+++ linux_rpm_install.sh
@@ -2,12 +2,12 @@ mkdir /home/"$USER"/KShell
 sudo dnf install python3
 sudo dnf install python3-tk
 sudo dnf install python3-pip
-pip3 install pyinstaller
+sudo pip3 install pyinstaller
 pyinstaller kshell.py
 mv * /home/"$USER"/KShell
 echo "Successfully moved files to directory /home/$USER/KShell/"
 cd /home/"$USER"/KShell/
 echo "You are now in the home directory of your KShell application!"
-echo "export PATH=/home/$USER/KShell/dist/kshell:$PATH" >> ~/.bashrc
+echo -e "export PATH=\x22$PATH\x22:/home/$USER/KShell/dist/kshell" >> ~/.bashrc
 echo "Launching KShell... to run, use the 'kshell' command from any directory in Terminal!"
 kshell
