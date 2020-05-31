f94c550652143ce42aa1b290647ea2efe07cac1a
microbitsarecool <66143785+microbitsarecool@users.noreply.github.com>
2020-05-31 13:54:30 -0600

    Updated mac_install
    Got rid of brew commands because they won't install tkinter

--- mac_install.sh
+++ mac_install.sh
@@ -1,16 +1,13 @@
 user=$(id -un)
 mkdir /home/"$user"/KShell
-xcode-select --install
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install python3
-brew install pip3
-pip3 install tkinter
-pip3 install pyinstaller
+pip install pyinstaller
 pyinstaller kshell.py
 mv * /home/"$user"/KShell
-echo "Successfully moved files to directory /home/$USER/KShell/"
+echo "Successfully moved files to directory /home/$user/KShell/"
 cd /home/"$user"/KShell
 echo "You are now in the home directory of your KShell application!"
-echo "export PATH=/home/$USER/KShell/dist/kshell:$PATH" >> ~/.profile
+echo "export PATH=/home/$user/KShell/dist/kshell:$PATH" >> ~/.profile
 echo "Launching KShell... to run, use the 'kshell' command from any directory in Terminal!"
+source ~/.profile
+"Sourced ~/.profile, beginning to run kshell..."
 kshell
