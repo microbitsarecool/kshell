53f08aaefe93a852ffa81cf409862e6d12602f16
microbitsarecool <66143785+microbitsarecool@users.noreply.github.com>
2020-05-31 14:12:57 -0600

    Uploaded documentation files and Windows installer
    Windows installer is a batch script and doc is simple txt file

---
+++ windows_install.bat
@@ -0,0 +1,5 @@
+mkdir c:\Users\%username%\KShell
+move * c:\Users\%username%\KShell
+echo "Successfully installed in your home directory (C:\Users\[user]\KShell)!"
+cd c:\Users\%username%\KShell
+echo "You are in the home directory of KShell! Do not move it or its files. You can add it to your Environment."
